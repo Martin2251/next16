d48728084dd00f2cccacbe14d1a91a00b5e93861
Martin Lowe <martinlowe673@gmail.com>
2025-10-26 18:54:46 +0000

    users

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -5,10 +5,10 @@ const Users = () => {
   return (
     <div>
       <ul>
- <li><Link href="/dashboard/users/1"></Link>User 1</li>
-  <li><Link href="/dashboard/users/2"></Link>User 2</li>
-   <li><Link href="/dashboard/users/3"></Link>User 3</li>
-    <li><Link href="/dashboard/users/4"></Link>User 4</li>
+ <li><Link href="/dashboard/users/1">1</Link>User 1</li>
+  <li><Link href="/dashboard/users/2">2</Link>User 2</li>
+   <li><Link href="/dashboard/users/3">3</Link>User 3</li>
+    <li><Link href="/dashboard/users/4">4</Link>User 4</li>
      <li>User 5</li>
       <li>User 6</li>
       </ul>
